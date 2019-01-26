d8e7d70c5383a313fc6afc9c982289790c268a39
GiorgosT97 <giorgos577@gmail.com>
2019-01-26 13:40:20 +0200

    Add get logger function to config module for returning a LOGGER configured object.

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 import logging
 
 FORMAT = '[%(asctime)s | %(levelname)s | %(name)s]: %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(format=FORMAT, level=logging.DEBUG,
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def get_logger(module_name):
+    """ Return a configured logger object. """
+    LOGGER = logging.getLogger(f'app:{module_name}')
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG,
+                        datefmt="%Y-%m-%d %H:%M:%S")
+
+    return LOGGER
